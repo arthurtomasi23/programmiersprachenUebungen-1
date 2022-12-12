db374df2bfd1c4f4db1832cb2f4af724d27e7abe
arthurtomasi23 <arthur.tomasi@gmx.de>
2022-12-12 19:32:37 +0100

    changes in javascript and css
    Made the numbers appear on the screen and set the position for the top left tile. Also chose font for the project.
    -->tried new approach for tile positioning (#tile1 { order: 1;}
    #game-board { display: flex;
    flex-wrap: wrap;}

--- finalProject/script.js
+++ finalProject/script.js
@@ -1,24 +1,29 @@
 //making the grid
-var rows = 3
-var columns = 3
+const rows = 3
+const columns = 3
 //labeling current Tile and the blank Tile the current Tile will switch to if clicked
-var currenTile;
-var blankTile; //one tile needs to be empty
-const numbers [1,2,3,4,5,6,7,8,9];
-var turns = 0;
+let currenTile;
+let blankTile; //one tile needs to be empty
+const numbers = [1,2,3,4,5,6,7,8,9];
+let turns = 0;
 
-var numbersOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const numbersOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 window.onload = function() {
-    for (let r=0; r < rows; r++) {
-        for (let c=0; c < columns, c++) {
+    console.log("working")
+    for (let r=0; r < numbers.length; r++) {
+        for (let c=0; c < columns; c++) {
         //creating the <div> tag
-            let tile = document.createElement("div");
+            const tile = document.createElement("div");
             //finding aout the position of the numbers inside the grid so you can only switch tiles which are next to each other
-            tile.id = r.toString()+ "-" + c.toString();
-            tile.src = numbersOrder.shift();
+            // tile.id = r.toString()+ "-" + c.toString();
+            // tile.src = numbersOrder.shift();
             //adding the numbers inside a div insinde the game-Board div
-            document.getElementsById("game-Board").append(tile);
+            tile.classList.add("tile")
+            tile.style.top = "0px"
+            tile.style.left = "0px"
+            tile.innerHTML = r
+            document.getElementById("game-Board").append(tile);
         }
     }
 }
