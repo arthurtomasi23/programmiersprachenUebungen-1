4ff9dfa30a46a7a755d9c3716c81b64667cbf66d
arthurtomasi23 <arthur.tomasi@gmx.de>
2022-12-14 15:25:31 +0100

    tiles are in the right order and numbers appear centered inside them
    all the tiles are appearing correctly and the numbers inside them also. I also cleaned up my code a bit.

--- finalProject/script.js
+++ finalProject/script.js
@@ -1,29 +1,25 @@
-//making the grid
-const rows = 3
-const columns = 3
+
 //labeling current Tile and the blank Tile the current Tile will switch to if clicked
 let currenTile;
 let blankTile; //one tile needs to be empty
-const numbers = [1,2,3,4,5,6,7,8,9];
 let turns = 0;
+let gridSize = 3
+//starting position of the tile
 
-const numbersOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 window.onload = function() {
     console.log("working")
-    for (let r=0; r < numbers.length; r++) {
-        for (let c=0; c < columns; c++) {
+    for (let r=0; r < gridSize; r++) {
+        for (let c=0; c < gridSize; c++) {
         //creating the <div> tag
             const tile = document.createElement("div");
-            //finding aout the position of the numbers inside the grid so you can only switch tiles which are next to each other
-            // tile.id = r.toString()+ "-" + c.toString();
-            // tile.src = numbersOrder.shift();
             //adding the numbers inside a div insinde the game-Board div
             tile.classList.add("tile")
-            tile.style.top = "0px"
-            tile.style.left = "0px"
-            tile.innerHTML = r
-            document.getElementById("game-Board").append(tile);
+            tile.style.top = r * (600/gridSize) + "px"
+            tile.style.left = c * (600/gridSize) +"px"
+            tile.innerHTML = r*gridSize +c
+            document.getElementById("gameBoard").append(tile);
         }
     }
 }
+
