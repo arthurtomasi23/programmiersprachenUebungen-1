0713dc73d5c075d4ed43e15beb75798d3e924bdc
arthurtomasi23 <arthur.tomasi@gmx.de>
2022-12-29 16:21:55 +0100

    i can swap only with the empty tile
    i can swap only the tile that is next to the tile with the name 9.jpg and with no other tile

--- finalProject/script2.js
+++ finalProject/script2.js
@@ -5,13 +5,14 @@ let currentTile; //the clicked tile
 let blankTile; //the blank tile
 let turns = 0;
 
-let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-//let imgOrder = ["2", "6", "3", "9", "5", "8", "4", "1", "7"]
+//let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let imgOrder = ["2", "6", "3", "9", "5", "8", "4", "1", "7"]
 
 window.onload = function() {
     for (let r=0; r < rows; r++) {
         for (let c=0; c < columns; c++) {
-
+            //that creates image id's for all the tiles(imagesnippets) 
+            //so i can later use them to find out if to tiles are next to each other
             let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
             tile.src = imgOrder.shift() + ".jpg";
@@ -49,9 +50,33 @@ function dragDrop() {
 }
 
 function dragEnd() {
+    if(!blankTile.src.includes("9.jpg")) {
+        return;
+    }
+    //finds the coordinates of the clicked tile
+    let currentCoordinates = currentTile.id.split("-"); //split() seperates the coordinates by the "-" -->now its an array of two 0s 
+    let r = parseInt(currentCoordinates[0]); //parseInt takes the first part of the array and makes it into a integer
+    let c = parseInt(currentCoordinates[1]); //and here the same with the second part of the array
+    
+    //finds the coordinates of the blank tile
+    let blankCoordinates = blankTile.id.split("-");
+    let r2 = parseInt(blankCoordinates[0]);
+    let c2 = parseInt(blankCoordinates[1]);
+
+    //setting the positions from witch we can swap with the blank tile
+    //the tiles have to be next to the blank tile(to the right/left/over or under the blank tile)
+    let toTheLeft = r == r2 && c2 == c-1;
+    let toTheRight = r == r2 && c2 == c+1;
+    let over = c == c2 && r2 == r-1;
+    let under = c == c2 && r2 == r+1;
+
+    let isNextTo = toTheLeft || toTheRight || over || under;
+
+    if(isNextTo) {
     let currentImg = currentTile.src;
     let blankImg = blankTile.src;
 
     currentTile.src = blankImg;
     blankTile.src = currentImg;
+    }
 }
